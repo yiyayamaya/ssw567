c4e862228c6ccaf4f0148e766912a680f1b3c683
yiyayamaya <wuxingjian98@gmail.com>
2022-09-14 14:25:11 -0400

    hw1

---
+++ hw1/hw.md
@@ -0,0 +1,11 @@
+What challenges did you encounter with this assignment, if any?   
+nothing
+
+What did you think about the requirements specification for this assignment?  
+clear and good
+
+What challenges did you encounter with the tools?  
+nothing
+
+Describe the criteria you used to determine that you had sufficient test cases, i.e. how did you know you were done?  
+by reviewing the requirements and designing test for edge cases
--- hw1/hw1.py
+++ hw1/hw1.py
@@ -2,14 +2,21 @@ import unittest
 
 
 def classifyTriangle(a, b, c):
-    list = [a, b, c]
-    list.sort()
-    a = list[0]
-    b = list[1]
-    c = list[2]
+    if not (isinstance(a, float) or isinstance(a, int)):
+        return "invalid input"
+    if not (isinstance(b, float) or isinstance(b, int)):
+        return "invalid input"
+    if not (isinstance(c, float) or isinstance(c, int)):
+        return "invalid input"
+
+    input_list = [a, b, c]
+    input_list.sort()
+    a = input_list[0]
+    b = input_list[1]
+    c = input_list[2]
 
     if a <= 0 or b <= 0 or c <= 0:
-        return "invalid side length"
+        return "invalid input"
     if a + b <= c:
         return "NotATriangle"
     if a ** 2 + b ** 2 == c ** 2:
@@ -24,15 +31,16 @@ def classifyTriangle(a, b, c):
 class TestTriangles(unittest.TestCase):
     def test_right(self):
         self.assertEqual(classifyTriangle(3, 4, 5), 'Right')
-        self.assertEqual(classifyTriangle(4, 3, 5), 'Right')
+        self.assertEqual(classifyTriangle(3, 4, 5.1), 'Right')
 
     def test_not_a_triangle(self):
         self.assertEqual(classifyTriangle(1, 1, 3), 'NotATriangle')
         self.assertEqual(classifyTriangle(1, 2, 3), 'NotATriangle')
 
     def test_invalid_input(self):
-        self.assertEqual(classifyTriangle(0, 4, 5), 'invalid side length')
-        self.assertEqual(classifyTriangle(-1, 3, 5), 'invalid side length')
+        self.assertEqual(classifyTriangle(0, 4, 5), 'invalid input')
+        self.assertEqual(classifyTriangle(-1, 3, 5), 'invalid input')
+        self.assertEqual(classifyTriangle("ss", 3, 5), 'invalid input')
 
     def test_equilateral(self):
         self.assertEqual(classifyTriangle(3, 3, 3), 'Equilateral')
